b55f000eb11adf1c907a011e8e1325c99e250ee1
Nico <nicolasmuniz2005@gmail.com>
2024-12-12 13:47:01 +0100

    Cambios algoritmos (falta arreglar el desbordamiento de Backtrack() )

--- Practica-4/algs.py
+++ Practica-4/algs.py
@@ -1,6 +1,40 @@
 #!env/bin/python
 
 
+# Backtracking function
+def Backtrack(T:list[list], n:int, m:int) -> str:
+    '''
+    Description
+    -----------
+    Searches for a valid path inside a table `T`.
+
+    Parameters
+    ----------
+    T : `list[list]`
+        Table in which to search.
+    n : `int`
+        Number of rows of T.
+    m : `int`
+        Number of columns of T.    
+
+    Returns
+    -------
+    `str`
+        Valid path inside the table 
+    '''
+    idx, jdx = -1
+    while (idx, jdx) != (-n,-m): # TODO : Desbordamiento de la tabla por accesos a elementos fuera de ella
+        if T[idx - 1][jdx]:
+            idx += -1
+            sol += "A"
+
+        if T[idx][jdx - 1]:
+            jdx += -1
+            sol += "A"
+
+    return sol
+
+
 # Mixture algorithms
 def MixtureDP (A:str|list, B:str|list, C:str|list)->bool:
     '''
@@ -32,35 +66,23 @@ def MixtureDP (A:str|list, B:str|list, C:str|list)->bool:
     if (n + m) != s:
         return False
 
-    t = [[False for _ in range(m+1) ] for _ in range(n+1) ]
-    t[0][0] = True
+    T = [[False for _ in range(m+1) ] for _ in range(n+1) ]
+    T[0][0] = True
 
     for i in range(n + 1):
         for j in range(m + 1):
-            t[i][j] = t[max(0, i-1)][j] or t[i][max(0, j-1)]
-            if t[i][j] and (i < n or j < m):
+            T[i][j] = T[max(0, i-1)][j] or T[i][max(0, j-1)]
+            if T[i][j] and (i < n or j < m):
                 k = i + j
-                t[i][j] = False
+                T[i][j] = False
 
                 if i < n:
-                    t[i][j] = t[i][j] or A[i] == C[k]
+                    T[i][j] = T[i][j] or A[i] == C[k]
 
                 if j < m:
-                    t[i][j] = t[i][j] or B[j] == C[k]
+                    T[i][j] = T[i][j] or B[j] == C[k]
 
-    # TODO : Recorrer tabla hacia atrás desde el final
-
-    for i in range(n, 0, -1):
-        for j in range(m, 0, -1):
-            if t[i][j]:
-                if B[j-1] == C[i+j -1]:
-                    sol += "B"
-                
-                if A[i-1] == C[i+j -1]:
-                    sol += "A"
-                    
-                
-    return sol[::-1]
+    return Backtrack(T, n, m)
 
 
 def MixtureCX(A:str|list, B:str|list, C:str|list) -> bool:
@@ -88,7 +110,7 @@ def MixtureCX(A:str|list, B:str|list, C:str|list) -> bool:
     m = len(B)
     s = len(C)
 
-    sol = ""
+    T = [[False for _ in range(m+1) ] for _ in range(n+1) ]
 
     if n + m != s:
         return False
@@ -100,26 +122,26 @@ def MixtureCX(A:str|list, B:str|list, C:str|list) -> bool:
         (i,j) = Trial.pop()
         k = i + j
 
-        if k >= s:
-            return sol
+        if k == s:
+            return Backtrack(T, n, m) # Returns a valid path searching through the DP table 
 
-        if i < n and A[i] == C[k] and (i+1,j) not in Known:
-            sol += "A"
-            Trial.append((i+1,j))
-            Known.add((i+1,j))
+        if i < n and A[i] == C[k] and (i+1, j) not in Known:
+            Trial.append((i+1, j))
+            Known.add((i+1, j))
 
-        if j < m and B[j] == C[k] and (i,j+1) not in Known:
-            sol += "B"
-            Trial.append((i,j+1))
-            Known.add((i,j+1))
+        if j < m and B[j] == C[k] and (i, j+1) not in Known:
+            Trial.append((i, j+1))
+            Known.add((i, j+1))
 
-    return False
+    else:
+        return False
+
 
 
 if __name__ == "__main__":
-    a = "Hello"
-    b = "World"
-    c = "WorldHello"
+    a = "aa"
+    b = "aaa"
+    c = "aaaaa"
 
     print(MixtureDP(a,b,c))
     print(MixtureCX(a,b,c))
